3407ab49f1294f9278c5c47708be297dfed1c5f0
Rukhsana Kanwal <rukhsanakanwal059@gmail.com>
2026-08-06 21:49:43 +0500

    Create script.js

---
+++ Day-4-JavaScript-Calculator/script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+function append(value){
+display.value += value;
+}
+function clearDisplay(){
+display.value = "";
+}
+function del(){
+display.value = display.value.slice(0,-1);
+}
+function calculate(){
+try{
+display.value = eval(display.value);
+}
+catch{
+display.value = "Error";
+}
+}
