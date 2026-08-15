85d95cd2350fda504f00809295b1f4c9575c81eb
Rukhsana Kanwal <rukhsanakanwal059@gmail.com>
2026-08-15 22:58:42 +0500

    Create Navbar.jsx

---
+++ Day-6-React/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Student Dashboard</h2>
+
+      <div className="nav-links">
+        <span>Dashboard</span>
+        <span>Courses</span>
+        <span>Profile</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
